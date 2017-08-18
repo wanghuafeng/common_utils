e57ecd6dd274549a3c0624825e14ee82bec68886
wanghuafeng <sivilwang@163.com>
2017-08-18 10:19:56 +0800

    add some proxy info

--- _requests.py
+++ _requests.py
@@ -9,7 +9,6 @@ url = 'http://www.baidu.com'
 
 
 class CookiesOpt(object):
-
     def cookie_opt(self):
         """cookie的相关操作"""
         session_obj.cookies.set('key', None)  # 清楚cookie中为key的值
@@ -24,3 +23,48 @@ class CookiesOpt(object):
             'key2': 'val2'
         }
         session_obj.get(url, cookies=cookies)
+
+
+class ProxiesOpt(object):
+
+    @property
+    def proxies(self):
+        """
+        http参数设置及使用方式
+        """
+        proxies = { # http/https设置同样代理
+            'all': '60.187.108.9:47586',
+        }
+        http_proxy = {
+            'http': '60.187.108.9:47586',  # http://60.187.108.9:47586
+        }
+        https_proxy = {
+            'https': '60.187.108.9:47586',  # http://60.187.108.9:47586
+        }
+        return proxies
+
+    def use_proxy(self):
+        requests.get(url, proxies=self.proxies)
+
+class RequestsOpt(object):
+    def _get(self):
+        """
+        get请求, 请求默认参数字段名称:params，
+        网络请求时，params中的参数将会以query string 形式传输
+        服务端的http server可以打印出以该方式传输的参数
+        """
+        params = {
+            'k': 'v'
+        }
+        requests.get(url, params=params)
+
+    def _post(self):
+        """
+        post请求，请求默认参数字段名称:data
+        网络请求时，data中的参数将会被放在 body 中传输
+        服务端http server不做特殊截获无法打印该部分参数
+        """
+        data = {
+            'k': 'v'
+        }
+        requests.post(url, data=data)
